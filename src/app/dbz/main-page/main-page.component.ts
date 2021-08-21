5a2fb0fd1f065e1e60809b56b7e95843f94e3990
Agustin Biagi <agustin.biagi@gmail.com>
2021-08-21 10:27:24 -0300

    Version con componente hijos video-61

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,50 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
-interface Personaje {
-  nombre:string;
-  poder:number
-}
-
+import { Personaje } from '../interfaces/dbz.interface';
 
 
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+	selector: 'app-main-page',
+	templateUrl: './main-page.component.html',
+	styleUrls: ['./main-page.component.css']
 })
 
 
-export class MainPageComponent  {
+export class MainPageComponent {
 
-  personajes: Personaje[]=[
-    {
-      nombre: "Goku",
-      poder: 15000
-    },
-    {
-      nombre: "Agustin",
-      poder: 45000
-    }
-  ];
+	personajes: Personaje[] = [
+		{
+			nombre: "Goku",
+			poder: 15000
+		},
+		{
+			nombre: "Agustin",
+			poder: 45000
+		}
+	];
 
-  nuevo:Personaje={
-    nombre:'',
-    poder: 0
-  } 
+	nuevo: Personaje = {
+		nombre: '',
+		poder: 0
+	}
 
-  agregar(){
-    if(this.nuevo.nombre.trim().length===0){
-      return;
-    }
-    this.personajes.push(this.nuevo);
-    this.nuevo={
-      nombre: '',
-      poder: 0
-    }
-  }
-
-  cambiarNombre(event:any){
-    console.log(event.target.value)
-  }
+	agregar() {
+		if (this.nuevo.nombre.trim().length === 0) {
+			return;
+		}
+		this.personajes.push(this.nuevo);
+		this.nuevo = {
+			nombre: '',
+			poder: 0
+		}
+	}
 
 }
---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,22 @@
+import { NullTemplateVisitor } from '@angular/compiler';
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+	selector: 'app-personajes',
+	templateUrl: './personajes.component.html',
+	styleUrls: ['./personajes.component.css']
+})
+
+export class PersonajesComponent {
+
+	@Input () personajes: Personaje[] = [];
+	@Input() nuevo: Personaje = {
+		nombre: '',
+		poder: 0
+	} 
+
+
+}
+
+
